8deaf731cd30d326ca7180a198ca454f695a6455
Cheyo <bstsnwhls@gmail.com>
2022-09-04 11:52:55 -0500

    loader-animation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./styles/App.css";
 import { useState } from "react";
 import Dog from "./components/Dog";
+import { MutatingDots } from "react-loader-spinner";
 
 function App() {
   const [dog, setDog] = useState(null);
+  const [load, setLoading] = useState(false); // Se declara el state inicial del loader
 
   const getDog = async () => {
+    // Se cmabia el estado a true para que el loader se renderice
+    setLoading(true);
+
     // Obtener respuesta de la API
     const api = await fetch("https://dog.ceo/api/breeds/image/random");
     // Obtener el contenido de la API en JSON (message[linkDeImagen] & status)
@@ -15,6 +20,9 @@ function App() {
     console.log(dogApi.message);
     // Cambiar el State de null a setDog al link de la API
     setDog(dogApi.message);
+
+    // Finalmente se regresa el estado al inicial para desmontar el componente
+    setLoading(false);
   };
 
   return (
@@ -34,14 +42,28 @@ function App() {
         See a cute dog
       </button>
 
-      {dog ? (
-        <Dog dogImage={dog} />
-      ) : (
+      {!load && !dog ? (
         <p className="dog_msj">Here will be your dog</p>
+      ) : load && !dog ? (
+        <div className="loader-container">
+          <MutatingDots
+            height="100"
+            width="100"
+            color="#4fa94d"
+            secondaryColor="#4fa94d"
+            radius="12.5"
+            ariaLabel="mutating-dots-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+          />
+        </div>
+      ) : (
+        <Dog dogImage={dog} />
       )}
     </div>
   );
-  // Se usan ternarios { x ? (true) : (false)} para el Conditional Rendering del componente que contiene el data de la API
+  // Se usan ternarios { x ? (true) : (false)} para el Conditional Rendering del componente que contiene el data de la API y el loader
 }
 
 export default App;
