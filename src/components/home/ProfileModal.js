f68f2ba4af8a6a43bac3887959faa346ade8ea11
Cheyo <bstsnwhls@gmail.com>
2022-10-09 16:34:07 -0500

    profile-modal-logic-not-working

--- src/components/home/ProfileModal.js
+++ src/components/home/ProfileModal.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../styles/ProfileModal.css";
 import { signOut } from "firebase/auth";
 import { auth } from "../../FirebaseConfig";
 import { useNavigate } from "react-router-dom";
+import ProfileSettings from "./ProfileSettings";
 
 function ProfileModal() {
   const navigate = useNavigate();
+
+  const [pSettings, setPSettings] = useState(false);
 
   const handleSignOut = (e) => {
     e.preventDefault();
@@ -23,12 +26,14 @@ function ProfileModal() {
     <div className="menu-container">
       <ul className="list-container">
         <li
+          className="profile_li"
           style={{
             borderBottom: "1.5px solid #f5f5f5",
             paddingTop: "7px",
             paddingBottom: "7px",
             paddingLeft: "10px",
           }}
+          onClick={(e) => setPSettings(true)}
         >
           Profile
         </li>
@@ -48,6 +53,7 @@ function ProfileModal() {
           </button>
         </li>
       </ul>
+      {pSettings && alert("hi")}
     </div>
   );
 }
---
+++ src/components/home/ProfileSettings.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ProfileSettings() {
+  return (
+    <div className="notLogged_container">
+      <div className="message_container">
+        <div className="notLoggedmsj_container">
+          <h1>Sorry!</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProfileSettings;
